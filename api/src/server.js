0897b0a8f17102bedfe0db17450c16018883d828
Nikolay Kiselev <kine4599@gmail.com>
2020-08-04 11:24:21 +0300

    tunning posgres docker #4

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,17 @@
 // Postgres Client Setup
 const { Pool } = require('pg');
+
+console.log('user:', process.env.POSTGRES_USER)
+console.log('host:', process.env.POSTGRES_HOST)
+console.log('database:', process.env.POSTGRES_DB)
+console.log('password:', process.env.POSTGRES_PASSWORD)
+console.log('port:', process.env.POSTGRES_PORT)
+console.log()
+
 const pgClient = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT
 });
