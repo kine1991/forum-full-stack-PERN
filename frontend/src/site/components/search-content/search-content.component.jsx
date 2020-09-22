f920c6ed305ddf7d7190c115cff869b257c7b9c0
Nikolay Kiselev <kine4599@gmail.com>
2020-09-22 23:14:59 +0300

    #41

--- frontend/src/site/components/search-content/search-content.component.jsx
+++ frontend/src/site/components/search-content/search-content.component.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 import { fetchChannelsByTerm } from 'redux/channel/channel.action';
 import { BehaviorSubject, Subject } from 'rxjs';
@@ -6,44 +7,49 @@ import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 import { SearchContentConteiner } from './search-content.styles';
 
-// function debounceTime(term, time) {
-
-// }
-
-// function debounce(func, wait = 100) {
-//   let timeout;
-//   return function(...args) {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//       func.apply(this, args);
-//     }, wait);
-//   };
-// }
-
-const test = () => {
-  console.log('test')
-}
 const subjectTerm$ = new Subject('');
+const useObservable = observable => {
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    let subscription = observable.subscribe(result => {
+      setValue(result);
+    });
 
-subjectTerm$.pipe(
-  debounceTime(3000),
-  distinctUntilChanged()
-).subscribe(value => {
-  console.log('value', value)
-})
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [observable, setValue]);
 
+  return value;
+}
 
 const SearchContent = React.forwardRef(({term}, ref) => {
+  const [channels, setChannels] = useState(null);
 
+  const termDelaed = useObservable(subjectTerm$.pipe(
+    debounceTime(1000),
+    distinctUntilChanged()
+  ));
+  
   useEffect(() => {
-    subjectTerm$.next(term)
+    subjectTerm$.next(term);
   }, [term]);
 
+  useEffect(() => {
+    fetchChannelsByTerm(termDelaed).then(response => {
+      console.log('response', response.data.channels);
+      setChannels(response.data.channels);
+    });
+  }, [termDelaed]);
 
-  if(!term) return <div></div>
+  if(!channels) return <div></div>
+  if(channels.length === 0) return <div>Не найдено</div>
   return (
     <SearchContentConteiner ref={ref}>
-      SearchContent <h1>{term}</h1>
+      <h1>Количество: {channels.length}</h1>
+      {channels.map(channel => (
+        <div key={channel.id}>{channel.id}</div>
+      ))}
     </SearchContentConteiner>
   )
 });
